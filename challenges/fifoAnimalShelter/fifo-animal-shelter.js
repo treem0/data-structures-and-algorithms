cc52e625af9b7dc6d8806ad907f34520b3467be8
Travis Molter <travismolter@gmail.com>
2019-12-18 17:31:23 -0800

    Started the animal shelter stuff. I am going about it 2 different ways, i think the second way might be the way to do it

---
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -0,0 +1,129 @@
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class DogStack {
+    constructor() {
+        this.top = null;
+    }
+  
+    push(dog) {
+        const node = new Node(dog, this.top);
+        this.top = node;
+    }
+  
+    pop() {
+        if(!this.top) return null;
+  
+        const top = this.top;
+        this.top = top.next;
+        return top;
+    }
+  
+    peek() {
+        if(!this.top) return null;
+        return this.top.value;
+    }
+}
+
+class CatStack {
+    constructor() {
+        this.top = null;
+    }
+  
+    push(cat) {
+        const node = new Node(cat, this.top);
+        this.top = node;
+    }
+  
+    pop() {
+        if(!this.top) return null;
+  
+        const top = this.top;
+        this.top = top.next;
+        return top;
+    }
+  
+    peek() {
+        if(!this.top) return null;
+        return this.top.value;
+    }
+}
+
+class AnimalShelter {
+    constructor() {
+        this.front = null;
+        this.back = null;
+    }
+  
+    enqueue(animal) {
+        const node = new Node(animal, null);
+        if(this.isEmpty()) {
+            this.front = node;
+        } else {
+            this.back.next = node;
+        }
+        this.back = node;
+    }
+  
+    dequeue() {
+        if(this.isEmpty()) return null;
+  
+        const front = this.front;
+        this.front = this.front.next;
+        if(this.isEmpty()) this.back = null;
+  
+        return front;
+    }
+  
+}
+
+        class Animal {
+            constructor(type) {
+                this.type = type;
+            }
+        }
+        
+        class Dog extends Animal {
+            constructor(type) {
+                super(type);
+                type = 'dog'
+            }
+        }
+        
+        class Cat extends Animal {
+            constructor(type) {
+                super(type);
+                type = 'cat'
+            }
+            
+        }
+
+        class AnimalShelter {
+            constructor(animal) {
+                if (animal !== 'dog' || animal !== 'cat')
+                return null;
+            }
+
+            enqueue(animal) {
+                const node = new Animal(type, null);
+                    if(this.isEmpty()) {
+                        this.front = node;
+                    } else {
+                        this.back.next = node;
+                    }
+                        this.back = node;
+                    }
+            dequeue() {
+                    if(this.isEmpty()) return null;
+                  
+                    const front = this.front;
+                    this.front = this.front.next;
+                    if(this.isEmpty()) this.back = null;
+                  
+                    return front;
+                }        
+            }
